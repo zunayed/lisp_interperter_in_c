92c2d8165cfb3b5c9bfbe8178bf83333c2eceecf
Zunayed Ali <zunayed@gmail.com>
2014-04-05 22:03:33 -0400

    initial commit

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int  main(int argc, char** argv){
+    puts("Hello World");
+    // When a C program returns 0 this indicates there have been no errors running the program.
+    return 0;
+}
